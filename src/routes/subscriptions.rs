ad849317c07cb6cb1011076d229e77684ab099a3
bilesimo <stang3tzzz@gmail.com>
2026-01-31 19:08:24 -0300

    feat: introduce tracing for observability and secure database credentials using secrecy.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,8 +10,28 @@ pub struct FormData {
     pub name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    if let Err(e) = sqlx::query!(
+    match insert_subscriber(&form, pool.get_ref()).await {
+        Ok(_) => HttpResponse::Ok(),
+        Err(_) => HttpResponse::InternalServerError(),
+    }
+    .finish()
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+async fn insert_subscriber(form: &FormData, pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -21,12 +41,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
+    .execute(pool)
     .await
-    {
-        print!("Failed to execute query: {}", e);
-        HttpResponse::InternalServerError().finish()
-    } else {
-        HttpResponse::Ok().finish()
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
