8586477f8e96599fb8c319ec8d307de25ba57af6
bilesimo <stang3tzzz@gmail.com>
2026-03-16 17:45:05 -0300

    refactor: extract email confirmation link parsing into a `TestApp` helper method.

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,4 +1,3 @@
-use reqwest::Url;
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -29,24 +28,14 @@ async fn the_link_returned_by_the_subscribe_returns_a_200_if_called() {
 
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
 
-    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+    let confirmation_links = app.get_confirmation_links(email_request);
 
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
-        assert_eq!(links.len(), 1);
-        links[0].as_str().to_owned()
-    };
-
-    let raw_confirmation_link = get_link(&body["html_content"].as_str().unwrap());
-    let mut confirmation_link = Url::parse(&raw_confirmation_link).unwrap();
-
-    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
-    confirmation_link.set_port(Some(app.port)).unwrap();
-
-    let response = app.client.get(confirmation_link).send().await.unwrap();
+    let response = app
+        .client
+        .get(confirmation_links.html)
+        .send()
+        .await
+        .unwrap();
 
     assert_eq!(response.status().as_u16(), 200);
 }
